cf4968d04312f9e6d9ab42c3f8097499f31790bc
sachin kumar <01krsachin@gmail.com>
2026-07-08 17:21:42 +0530

    Update sorting_hat.py

--- day-3/sorting_hat.py
+++ day-3/sorting_hat.py
@@ -1,48 +1,48 @@
-"""
-Instructions
-The Sorting Hat is a magical talking hat at Hogwarts School of Witchcraft and Wizardry. The hat decides which of the four "Houses" each first-year student goes to:
+# """
+# Instructions
+# The Sorting Hat is a magical talking hat at Hogwarts School of Witchcraft and Wizardry. The hat decides which of the four "Houses" each first-year student goes to:
 
-🦁 Gryffindor
-🦅 Ravenclaw
-🦡 Hufflepuff
-🐍 Slytherin
+# 🦁 Gryffindor
+# 🦅 Ravenclaw
+# 🦡 Hufflepuff
+# 🐍 Slytherin
 
 
-Write a program that asks the user some questions with the int() and input() functions:
+# Write a program that asks the user some questions with the int() and input() functions:
 
-Q1) Do you like Dawn or Dusk?
-    1) Dawn
-    2) Dusk
+# Q1) Do you like Dawn or Dusk?
+#     1) Dawn
+#     2) Dusk
 
-If answer is equal to 1, Gryffindor and Ravenclaw both get a +1.
-Else if answer is equal to 2, Hufflepuff and Slytherin both get a +1.
-Else, output the message "Wrong input."
-Q2) When I’m dead, I want people to remember me as:
-    1) The Good
-    2) The Great
-    3) The Wise
-    4) The Bold
+# If answer is equal to 1, Gryffindor and Ravenclaw both get a +1.
+# Else if answer is equal to 2, Hufflepuff and Slytherin both get a +1.
+# Else, output the message "Wrong input."
+# Q2) When I’m dead, I want people to remember me as:
+#     1) The Good
+#     2) The Great
+#     3) The Wise
+#     4) The Bold
 
-If the answer is 1, Hufflepuff +2.
-Else if answer is 2, Slytherin +2.
-Else if answer is 3, Ravenclaw +2.
-Else if answer is 4, Gryffindor +2.
-Else, output the message "Wrong input."
-Q3) Which kind of instrument most pleases your ear?
-    1) The violin
-    2) The trumpet
-    3) The piano
-    4) The drum
+# If the answer is 1, Hufflepuff +2.
+# Else if answer is 2, Slytherin +2.
+# Else if answer is 3, Ravenclaw +2.
+# Else if answer is 4, Gryffindor +2.
+# Else, output the message "Wrong input."
+# Q3) Which kind of instrument most pleases your ear?
+#     1) The violin
+#     2) The trumpet
+#     3) The piano
+#     4) The drum
 
-If the answer is 1, Slytherin +4.
-Else if the answer is 2, Hufflepuff +4.
-Else if the answer is 3, Ravenclaw +4.
-Else if the answer is 4, Gryffindor +4.
-Else, output "Wrong input."
-Lastly, print out the score for each house.
+# If the answer is 1, Slytherin +4.
+# Else if the answer is 2, Hufflepuff +4.
+# Else if the answer is 3, Ravenclaw +4.
+# Else if the answer is 4, Gryffindor +4.
+# Else, output "Wrong input."
+# Lastly, print out the score for each house.
 
-Bonus: If you want to go further, see if you can figure out how to print out the house with the most points!
-"""
+# Bonus: If you want to go further, see if you can figure out how to print out the house with the most points!
+# """
 
 
 
