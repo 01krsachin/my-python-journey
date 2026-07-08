d4edd63595abdde0a0e6c9dc2b05b0b4da947dee
sachin kumar <01krsachin@gmail.com>
2026-07-08 17:57:01 +0530

    Update Magic_8_Ball.py

--- day-2/Magic_8_Ball.py
+++ day-2/Magic_8_Ball.py
@@ -1,16 +1,16 @@
-The Magic 8 Ball is a popular office toy and children's toy invented in the 1940s for fortune-telling and advice seeking. 🎱
+# The Magic 8 Ball is a popular office toy and children's toy invented in the 1940s for fortune-telling and advice seeking. 🎱
 
-It's an oversized 8 ball with some of the following answers:
+# It's an oversized 8 ball with some of the following answers:
 
-Yes - definitely.
-It is decidedly so.
-Without a doubt.
-Reply hazy, try again.
-Ask again later.
-Better not tell you now.
-My sources say no.
-Outlook not so good.
-Very doubtful.
+# Yes - definitely.
+# It is decidedly so.
+# Without a doubt.
+# Reply hazy, try again.
+# Ask again later.
+# Better not tell you now.
+# My sources say no.
+# Outlook not so good.
+# Very doubtful.
 
 
 import random 
