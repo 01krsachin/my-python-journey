15bfe52c2a200f04850f7ca332b1052036998c46
sachin kumar <01krsachin@gmail.com>
2026-07-09 13:24:39 +0530

    Add PIN validation for bank access

---
+++ day-4/Bank_pass.py
@@ -0,0 +1,7 @@
+print("Welcome to the Bank of Sachin")
+pin = int(input("ENter your PIN: "))
+while pin != 1234:
+  pin = int(input("Incorrect PIN, Enter your PIN again: "))
+
+if pin ==1234:
+  print("pin accepted!🌻🌷")
